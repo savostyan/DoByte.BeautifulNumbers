150f330fd8b397442afba7124037441820d02972
savostyan <savostyan@yandex.ru>
2023-01-30 12:47:47 +0300

    Removed sums collection printing

--- DoByte.BeautifulNumbers/Program.cs
+++ DoByte.BeautifulNumbers/Program.cs
@@ -5,9 +5,4 @@ var helper = new BeautifulNumbersHelper();
 helper.MakeSumsDistribution();
 helper.CalculateBeautifulNumbers();
 
-foreach (var sum in helper.SumsCounters)
-{
-    Console.WriteLine($"sum: {sum.Key}, count: {sum.Value}");
-}
-
 Console.WriteLine($"Beautiful numbers count: {helper.BeautifulNumbersCount}");
