1dd598dc589fab23c5aa4028058e45f94fa55f8c
savostyan <savostyan@yandex.ru>
2023-01-30 12:43:47 +0300

    Test is done!

---
+++ DoByte.BeautifulNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using DoByte.BeautifulNumbers;
+
+var helper = new BeautifulNumbersHelper();
+
+helper.MakeSumsDistribution();
+helper.CalculateBeautifulNumbers();
+
+foreach (var sum in helper.SumsCounters)
+{
+    Console.WriteLine($"sum: {sum.Key}, count: {sum.Value}");
+}
+
+Console.WriteLine($"Beautiful numbers count: {helper.BeautifulNumbersCount}");
